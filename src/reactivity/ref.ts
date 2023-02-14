10abdc2ffac8b8bf6b78079089a2a1845bd82833
milkkky <2206002917@qq.com>
2023-02-14 16:09:11 +0800

    isRef和unRef

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -6,6 +6,7 @@ class RefImpl {
   private _value: any;
   public dep;
   private _rawValue: any;
+  public __v_isRef = true;
   constructor(value) {
     this._rawValue = value;
     this._value = convert(value);
@@ -43,4 +44,12 @@ function trackRefValue(ref) {
 }
 export function ref(value) {
   return new RefImpl(value);
+}
+export function isRef(ref) {
+  return !!ref.__v_isRef;
+}
+
+export function unRef(ref) {
+  //看看是不是ref-->ref.value
+  return isRef(ref) ? ref.value : ref;
 }
