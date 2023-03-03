7e1f78cf5267a0e84d7b32666c7ab9f51a533c48
milkkky <2206002917@qq.com>
2023-03-03 17:02:08 +0800

    compare

---
+++ example/componentEmit/App.js
@@ -0,0 +1,26 @@
+import { h } from "../../lib/mini-vue.esm.js";
+import { Foo } from "./Foo.js";
+
+
+
+export const App = {
+  name: "App",
+  render() {
+    // emit
+    return h("div", {}, [
+      h("div", {}, "App"),
+      h(Foo, {
+        onAdd() {
+          console.log("onAdd");
+        },
+        onAddFoo() {
+          console.log("onAddFoo");
+        },
+      }),
+    ]);
+  },
+
+  setup() {
+    return {};
+  },
+};
